0e0ac747e484c24a430a02ed1ef769aecbb2e630
repeto <repeto1204@gmail.com>
2022-03-20 20:14:34 +0900

    fix: build error fix (type error)

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,6 @@
-import { AnyAction, configureStore, Middleware, MiddlewareArray } from '@reduxjs/toolkit';
+import { AnyAction, configureStore, MiddlewareArray } from '@reduxjs/toolkit';
 import rootReducer, { RootReducer } from './rootReducer';
-import createSagaMiddleware from '@redux-saga/core';
+import createSagaMiddleware from 'redux-saga';
 import { createWrapper } from 'next-redux-wrapper';
 import logger from 'redux-logger';
 import rootSaga from './rootSaga';
@@ -10,7 +10,7 @@ const isDev = process.env.NODE_ENV === 'development';
 const makeStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = isDev ? [sagaMiddleware, logger] : [sagaMiddleware];
-  const store = configureStore<RootReducer, AnyAction, MiddlewareArray<Middleware>>({
+  const store = configureStore<RootReducer, AnyAction, MiddlewareArray<RootReducer>>({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false }).concat([...middlewares]),
     devTools: isDev
